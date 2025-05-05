56836edaa82dbb24aab771a3ced195def39335b3
Aleadinglight <tam.nguyen@mapbox.com>
2025-05-05 10:36:50 +0300

    Add workflow for publishing website to github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://aleadinglight.github.io',
+  base: 'blog',
   integrations: [mdx(), sitemap()],
   
   markdown: {
